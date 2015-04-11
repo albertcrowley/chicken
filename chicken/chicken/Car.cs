0ad0eb6a5dc6de907b270d1fac6e33739cd59a1a
Al <al@bwnj.net>
2015-04-11 17:13:27 -0400

    First try

---
+++ chicken/chicken/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+
+namespace chicken
+{
+    class Car
+    {
+        public int lane = 1;
+        Vector2 position = new Vector2(200, 200);
+        public int NUM_LANES = 3;
+        public int LANE_WIDTH = 100;
+        public int TOP_LANE_Y = 90;
+        public float carAngle = MathHelper.Pi / 2;
+
+        private Texture2D carTexture;
+        private KeyboardState oldKeyState = Keyboard.GetState();
+
+        int MAX_DELTA = 5;
+
+        public Car(Texture2D texture, int x = 200, int y=-1)
+        {
+            position.X = x;
+            position.Y = (y==-1) ? 140 + lane * 100: y;
+            carTexture = texture;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // gather input
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Down) && oldKeyState.IsKeyUp(Keys.Down))
+            {
+                lane = Math.Min(2, lane + 1);
+            }
+            if (state.IsKeyDown(Keys.Up) && oldKeyState.IsKeyUp(Keys.Up))
+            {
+                lane = Math.Max(0, lane - 1);
+            }
+            oldKeyState = state;
+
+            //move car
+            int desiredPosition = 140 + lane * 100;
+            float diff = position.Y - desiredPosition;
+            if (Math.Abs(diff) < MAX_DELTA)
+            {
+                position.Y = desiredPosition;
+                carAngle = MathHelper.Pi / 2;
+            }
+            else
+            {
+                int direction = (diff > 0) ? -1 : 1;
+
+                position.Y += direction * MAX_DELTA;
+
+                carAngle = (MathHelper.Pi / 2) + MathHelper.Pi / 20 * direction;
+            }
+
+        }
+
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Draw(carTexture, new Vector2(200 + 100 * lane, 240), Color.White);
+
+            spriteBatch.Draw(carTexture, position, null, Color.White, carAngle, new Vector2(0, 0), new Vector2(1, 1), SpriteEffects.None, 0);
+
+        }
+
+
+    }
+}
---
+++ chicken/chicken/Road.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace chicken
+{
+    class Road
+    {
+        Texture2D line;
+        Texture2D edge;
+
+        float SPEED = 400;
+        int HORIZON = 1000;
+        int nextLine = 0;
+        int lineGap = 250;
+        int LANE_1 = 120;
+        int LANE_2 = 230;
+        int LANE_3 = 330;
+        int LANE_4 = 430;
+        float distance = 0;
+        List<int> linePositions = new List<int>();
+
+
+        public Road(Texture2D _line, Texture2D _edge)
+        {
+            line = _line;
+            edge = _edge;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            distance = SPEED * (float) (gameTime.TotalGameTime.TotalMilliseconds /1000);
+            if (distance + HORIZON > nextLine)
+            {
+                linePositions.Add(nextLine);
+                nextLine += lineGap;
+            }
+
+            // drop old lines
+            for (int i = 0; i < linePositions.Count; i++)
+            {
+                if (linePositions[i] <0) {
+                    linePositions.RemoveAt(i);
+                }
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+            for (int i = 0; i < linePositions.Count; i++)
+            {
+                spriteBatch.Draw(edge, new Rectangle((int)(linePositions[i] - distance), LANE_1, lineGap, 20), Color.White);
+                spriteBatch.Draw(line, new Vector2(linePositions[i] - distance, LANE_2), Color.White);
+                spriteBatch.Draw(line, new Vector2(linePositions[i] - distance, LANE_3), Color.White);
+                spriteBatch.Draw(edge, new Rectangle((int)(linePositions[i] - distance), LANE_4, lineGap, 20), Color.White);
+            }
+        }
+    }
+}
